70e02fdf6bf679182b7ae8290bce4e0e5fa9f11c
fvtimal <fatimal4tif@gmail.com>
2026-07-13 13:04:59 +0500

    added user reg and fixed errors

--- routers/users.py
+++ routers/users.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from database import users
+from schemas import UserRegister
+from utils import hash_password
 
 router = APIRouter(
     prefix="/users",
@@ -10,4 +13,32 @@ router = APIRouter(
 async def test():
     return {
         "message": "Users router working!"
+    }
+
+@router.post("/register")
+async def register(user: UserRegister):
+
+    existing_user = await users.find_one(
+        {"email": user.email}
+    )
+
+    if existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="Email already registered"
+        )
+
+    hashed = hash_password(user.password)
+
+    new_user = {
+        "username": user.username,
+        "email": user.email,
+        "password": hashed
+    }
+
+    result = await users.insert_one(new_user)
+
+    return {
+        "message": "User registered successfully",
+        "id": str(result.inserted_id)
     }
